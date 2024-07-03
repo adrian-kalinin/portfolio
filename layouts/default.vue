90ac2e133cec658bc11d2eadb73c6b73dc4efb52
Adrian Kalinin <adriankalinin@protonmail.com>
2024-07-03 19:12:38 +0300

    Add SEO and Meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+      crossorigin: '',
+    },
+  ],
+})
+</script>
+
 <template>
   <div class="mx-auto max-w-2xl">
     <TheHeader />
@@ -6,3 +22,9 @@
     </main>
   </div>
 </template>
+
+<style>
+body {
+  font-family: 'Roboto';
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Adrian Kalinin',
+  description: 'Software engineer from Helsinki.',
+  ogTitle: 'Adrian Kalinin',
+  ogDescription: 'Software engineer from Helsinki.',
+  twitterTitle: 'Adrian Kalinin',
+  twitterDescription: 'Software engineer from Helsinki.',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.ico',
+    },
+  ],
+})
+</script>
+
 <template>
   <div>
     Hello, World!
