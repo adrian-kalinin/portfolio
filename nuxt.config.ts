53f0c28f2f4319f2354bfeb8941274b35959e2c5
Adrian Kalinin <adriankalinin@protonmail.com>
2024-07-06 17:22:14 +0300

    Add code syntax highlighting in nuxt content files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,11 @@ export default defineNuxtConfig({
   colorMode: {
     classSuffix: '',
   },
+  content: {
+    highlight: {
+      theme: {
+        default: 'monokai',
+      },
+    },
+  },
 })
