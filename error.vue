a9b5030090a36f0962858db90f7fa6d103413112
Adrian Kalinin <adriankalinin@protonmail.com>
2025-01-04 15:59:34 +0200

    Add error page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  error: Object,
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <section>
+      <h1 class="text-4xl mb-4">
+        Error {{ error.statusCode }}
+      </h1>
+      <p class="text-xl text-gray-500 dark:text-gray-400">
+        {{ error.statusMessage }}
+      </p>
+    </section>
+  </NuxtLayout>
+</template>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,5 +1,13 @@
 <template>
   <article class="prose dark:prose-pre:bg-gray-900 dark:prose-invert">
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <h1>Document not found (404)</h1>
+        <p>Blog post could not be found...</p>
+      </template>
+      <template #default="{ doc }">
+        <ContentRenderer :value="doc" />
+      </template>
+    </ContentDoc>
   </article>
 </template>
