a88c7ab20b04bab93153ebf4bd4521ef16075c80
Adrian Kalinin <adriankalinin@protonmail.com>
2025-01-10 10:19:09 +0200

    Fix eslint formatting issues

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,9 @@ useHead({
 
 <template>
   <section>
-    <h1 class="text-4xl mb-6">Hey there!</h1>
+    <h1 class="text-4xl mb-6">
+      Hey there!
+    </h1>
     <p>My name is Adrian, and I'm a product-focused software engineer based in Helsinki, Finland.</p>
   </section>
 </template>
